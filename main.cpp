ef03b2bdb2270a4c1f208abd68eb96616159c9b9
0x0ali0 <147086357+0x0ali0@users.noreply.github.com>
2023-10-06 00:02:20 +0300

    Update4 main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,51 @@
 #include "bmplib.h"
 using namespace std;
 unsigned char imag[SIZE][SIZE];
-
+unsigned char imag2[SIZE][SIZE];
+unsigned char imag3[SIZE][SIZE];
 void loadImage();
+void loadImage2();
 void yourFilter();
-void blackAndWhite();
 void InvertImage();
+void MergeImages();
 void saveImage();
+void blackAndWhite();
+void saveImage2();
 
 int main() {
-loadImage();
-blackAndWhite();
-saveImage();
+    int n;
+    cout<<"chose the number of filter"<<endl;
+    cout<<"1-black and white"<<endl;
+    cout<<"2-invers image"<<endl;
+    cout<<"3-mare image"<<endl;
+    cin>>n;
+    if(n==1){
+        loadImage();
+        blackAndWhite();
+        saveImage();
+    }
+    if(n==2){
+        loadImage();
+        InvertImage();
+        saveImage();
+    }
+    if(n==3){
+        loadImage();
+        loadImage2();
+        MergeImages();
+        saveImage2();
+
+
+    }
+    return 0;
+
+}
+void loadImage2() {
+    char imageName[100];
+    cout << "enter your SOCAND image file sourse";
+    cin >> imageName;
+    strcat(imageName, ".bmp");
+    readGSBMP(imageName, imag2);
 }
 void loadImage(){
     char imageName[100];
@@ -31,6 +65,14 @@ void saveImage(){
     writeGSBMP(imageName,imag);
 
 
+
+}
+void saveImage2() {
+    char imageName[100];
+    cout << "enter your image name after edit";
+    cin >> imageName;
+    strcat(imageName, ".bmp");
+    writeGSBMP(imageName,imag3);
 }
 
 void yourFilter() {
@@ -40,20 +82,18 @@ void yourFilter() {
     }
 }
 
-        void blackAndWhite() {
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (imag[i][j] > 127) {
-                        imag[i][j] = 255;
-                    } else {
-                        imag[i][j] = 0;
-                    }
-                }
+void blackAndWhite() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (imag[i][j] > 127) {
+                imag[i][j] = 255;
+            } else {
+                imag[i][j] = 0;
             }
-
-
+        }
     }
- void InvertImage()
+}
+    void InvertImage()
     {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -61,3 +101,15 @@ void yourFilter() {
             }
         }
     }
+void MergeImages(){
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+          imag3[i][j]=(imag[i][j]+imag2[i][j])/2;
+
+        }
+
+    }
+}
+
+           
